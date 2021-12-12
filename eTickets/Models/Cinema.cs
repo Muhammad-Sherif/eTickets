3949d1c2bce6ac04f36b6458cc063199f6ab9993
Muhammad-Sherif <muhammadsherif.wo@gmail.com>
2021-12-12 22:17:03 +0200

    Adding Application models

---
+++ eTickets/Models/Cinema.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Models
+{
+	public class Cinema
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public byte[] Logo{ get; set; }
+
+
+
+	}
+}
---
+++ eTickets/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Models
+{
+	public class Movie
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Story { get; set; }
+
+		public Double Price { get; set; }
+		public byte[] Poster { get; set; }
+		public DateTime StartDate { get; set; }
+		public DateTime EndDate { get; set; }
+
+	}
+}
