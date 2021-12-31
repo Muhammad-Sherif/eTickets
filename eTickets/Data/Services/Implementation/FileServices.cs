bebb352d65c5ef74a6958dbd078b283508a58872
Muhammad-Sherif <muhammadsherif.wo@gmail.com>
2021-12-31 22:12:20 +0200

    Add file services

---
+++ eTickets/Data/Services/Implementation/FileServices.cs
@@ -0,0 +1,21 @@
+﻿using eTickets.Data.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Services.Implementation
+{
+	public class FileServices : IFileServices
+	{
+		public byte[] ConvertToByteArray(IFormFile file)
+		{
+			using var dataStream = new MemoryStream();
+			file.CopyTo(dataStream);
+			return dataStream.ToArray();
+
+		}
+	}
+}
---
+++ eTickets/Data/Services/Interfaces/IFileServices.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Services.Interfaces
+{
+	public interface IFileServices
+	{
+		byte[] ConvertToByteArray(IFormFile file);
+	}
+}
