756c2c06240202d41952250f2fa9e6968c1d1bdc
Muhammad Sherif <muhammadsherif.wo@gmail.com>
2022-02-09 14:52:14 +0200

    Move confiugrations folder to data folder

---
+++ eTickets/Data/Configurations/EntitiesConfigurations/CienmaConfigurations.cs
@@ -0,0 +1,26 @@
+﻿using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Configurations.EntitiesConfigurations
+{
+	public class CienmaConfigurations : IEntityTypeConfiguration<Cinema>
+	{
+		public void Configure(EntityTypeBuilder<Cinema> builder)
+		{
+			TableConfigurations(builder);
+		}
+		private void TableConfigurations(EntityTypeBuilder<Cinema> builder)
+		{
+			builder.Property(c => c.Name).IsRequired().HasMaxLength(250);
+			builder.Property(c => c.Description).IsRequired().HasMaxLength(2500);
+			builder.Property(c => c.Logo).IsRequired();
+
+		}
+	}
+
+}
---
+++ eTickets/Data/Configurations/EntitiesConfigurations/GenreConfigurations .cs
@@ -0,0 +1,22 @@
+﻿using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Configurations.EntitiesConfigrations
+{
+	public class GenreConfigurations : IEntityTypeConfiguration<Genre>
+	{
+		public void Configure(EntityTypeBuilder<Genre> builder)
+		{
+			TableConfigurations(builder);
+		}
+		private void TableConfigurations(EntityTypeBuilder<Genre> builder)
+		{
+			builder.Property(g => g.Name).IsRequired().HasMaxLength(250);
+		}
+	}
+}
